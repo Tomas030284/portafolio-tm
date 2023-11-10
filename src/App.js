63230eefb4afc73b56dc2614bd2af287ed06fa27
Tomas030284 <tomas.mogrovejo.acosta@gmail.com>
2023-11-10 13:32:39 -0500

    Fist Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Curriculum } from "./components/Curriculum";
+import { MisRutas } from "./router/MisRutas";
+
+function App() {
+  return (
+    <div className="layout">
+    
+      <MisRutas/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      Portafolio Tomás Mogrovejo Acosta &copy; Máster en React - 2023
+    </footer>
+  );
+}
