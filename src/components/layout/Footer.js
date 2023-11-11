174014744ffd273b9083c20827f776e9f46c83d8
Tomas030284 <tomas.mogrovejo.acosta@gmail.com>
2023-11-10 22:58:25 -0500

    Improving the footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className="footer">
-      Tomás Mogrovejo Acosta Portfolio &copy; React Master - 2023
+      Tomás Mogrovejo Acosta Portfolio &copy; 2023
     </footer>
   );
 }
